f7086e768d9e766a03a2b5dc7f734d4cfeff3a90
shrikant-88 <137672925+shrikant-88@users.noreply.github.com>
2023-06-25 11:40:10 +0530

    Update moves_Zeros.java

--- moves_Zeros.java
+++ moves_Zeros.java
@@ -9,13 +9,15 @@ public class moves_Zeros {
       for(int i =0;i<n;i++){
         arr[i] = sc.nextInt();
       }
-      for(int i = 0 ; i<n-1 ; i++){
+      for(int j = 0 ; j<n-1 ; j++ ){
+      for(int i = 0 ; i<n-1-j ; i++){
         if(arr[i] == 0){
             int tamp = arr[i];
             arr[i] = arr[i+1];
             arr[i+1] = tamp;
         }
       }
+    }
       for(int ele:arr){
         System.out.print(ele);
       }
